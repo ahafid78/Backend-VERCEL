45c3ce65f8b5250920caf27264cac14449285255
abdelhafid <ahafid78@gmail.com>
2024-07-02 09:31:39 +0100

    test-vercel

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+// const morgan = require("morgan");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+
+const sequelize = require("./src/db/sequelize");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Ne pas oublier l'installation des middlewares (dans le terminal avec la commande : npm install nom-middleware --save-dev) et l'importation avec : (const nom-middleware = require('nom-depende-middleware'))
+app
+  .use(favicon(__dirname + "/favicon.ico"))
+  // .use(morgan("dev"))
+  .use(bodyParser.json());
+
+sequelize.initDb();
+
+app.get("/", (req, res) => {
+  res.json("Hello, Heroku !");
+});
+
+// Ici, nous placerons nos futurs points de terminison
+
+require("./src/routes/findAllPokemons")(app);
+require("./src/routes/findAllPokemonByPK")(app);
+require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
+require("./src/routes/login")(app);
+
+// On ajoute la gestion des erreurs 404
+
+// app.use(({ res }) => {
+//   const message =
+//     "Impossible de trouver la ressource demandée ! Vous pouvez essayer ube autre URL";
+//   res.status(404).json({ message });
+// });
+
+const errorHandler = (req, res, next) => {
+  const message =
+    "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL";
+  res.status(404).json({ message, data: error });
+  next();
+};
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(
+    `Notre application Node est démarrée sur : http://localhost:${port}`
+  )
+);
---
+++ src/routes/findAllPokemons.js
@@ -0,0 +1,66 @@
+const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
+const auth = require("../auth/auth");
+
+module.exports = (app) => {
+  app.get("/api/pokemons", auth, (req, res) => {
+    if (req.query.name) {
+      const name = req.query.name;
+      const limit = parseInt(req.query.limit) || 5;
+
+      if (name.lenth < 2) {
+        const message =
+          "Le terme de recherche doit contenir au moins 02 caracteres";
+        return res.status(400).json({ message });
+      }
+
+      return Pokemon.findAndCountAll({
+        where: {
+          name: {
+            // 'name' est la propriété du modèle pokémon
+            // [Op.eq]: name,  'name' est le critère de la recherche
+            [Op.like]: `%${name}%`, // 'name' est le critère de la recherche
+            // Explication:
+            // On recherche un pokémon qui commence par le terme de recherche :  ${name}%
+            // On recherche un pokémon qui qui se termine par le terme de recherche :  %${name}
+            // On recherche un pokémon qui qui contient le terme de recherche (notre cas):  %${name}%
+          },
+        },
+        order: ["name"],
+        limit: limit, // limiter les résultats dynamiquement
+        // Explication:
+        // L'utilisateur souhaite récupérer 5 pokémons
+        // mon-site.com/api/pokemons?limit=5
+
+        // L'utilisateur souhaite récupérer 10 pokémons
+        // mon-site.com/api/pokemons?limit=10
+
+        // L'utilisateur souhaite récupérer 1 pokémons
+        // mon-site.com/api/pokemons?limit=1
+
+        // Par défault, la limite est à 5 pokémons
+        // mon-site.com/api/pokemons
+      }).then(({ count, rows }) => {
+        const message = `Il y a ${count} pokemons qui corresponde aux termes de recherche ${name} `;
+        res.json({ message, data: rows });
+      });
+    } else {
+      Pokemon.findAll({ order: ["name"] })
+        //Explication:
+        //Cas1: On tri par ordre alphabétique croissant, sur la propriété "name" : ['name','ASC']
+        //Cas2: On tri par ordre alphabétique décroissant, sur la propriété "name" : ['name','DESC']
+
+        //Cas3: On tri par ordre alphabétique croissant, sur la propriété "name",
+        //(par default) en utilisant un raccourci de syntaxe. Identique au cas n°1 ! : ['name']
+
+        .then((pokemons) => {
+          const message = "La liste des pokémons a bien été récupérée.";
+          res.json({ message, data: pokemons });
+        })
+        .catch((error) => {
+          const message = `La liste na pas pue être récupérée, réessayez dans quelques instants`;
+          res.status(500).json({ message, data: error });
+        });
+    }
+  });
+};
